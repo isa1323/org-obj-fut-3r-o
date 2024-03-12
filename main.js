46aa1b010886adc8c99621dff4b019273bffd4ca
isabella <105920830+isa1323@users.noreply.github.com>
2024-03-12 10:17:53 -0300

    mainjs

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const botoes = document.querySelectorAll(".botao");
 
-for (let i = o; i < botoes.length; j++){
+for (let i = 0; i < botoes.length; j++){
     botoes[i].onclick = function(){
         for(let j = 0; j < botoes.length; j++){
             botoes[j].classList.remove(ativo);
